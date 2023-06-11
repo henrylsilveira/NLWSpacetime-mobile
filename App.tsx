d20b6720a1196fd4c02d48a1d092cf2572d7f86b
Henry Leão <henrylsilveira@gmail.com>
2023-06-11 09:38:00 -0400

    First Commit

---
+++ App.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native'
+
+export default function App() {
+  return (
+    <View className="bg-gray-950 flex-1 items-center justify-center">
+      <Text className="text-zinc-50 font-bold text-5xl">Hello World</Text>
+    </View>
+  )
+}
